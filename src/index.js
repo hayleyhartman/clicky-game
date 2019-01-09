71a1457c47bd0e6729745ec525c4a94540ea7819
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2019-01-08 19:57:48 -0500

    build2

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />,<Router basename={process.env.PUBLIC_URL}></Router>, document.getElementById("root"));
