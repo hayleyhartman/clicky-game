adf41f9ac6c8d5c71c7020fe73d11d456bfb935e
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2019-01-08 20:08:09 -0500

    Updates

---
+++ precache-manifest.8deabcd4d74785fab3a7bff6b17c2c22.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "34bb207dd165b3200f8fd52f0251cadb",
+    "url": "/clicky-game/static/media/s2.34bb207d.jpg"
+  },
+  {
+    "revision": "1635b5a67d81f6443052",
+    "url": "/clicky-game/static/css/main.3efb03f2.chunk.css"
+  },
+  {
+    "revision": "ebb74506a302c5af88b05ae7e0d4bde2",
+    "url": "/clicky-game/static/media/s9.ebb74506.jpg"
+  },
+  {
+    "revision": "03d95aa1677d19ee094e",
+    "url": "/clicky-game/static/js/1.03d95aa1.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "ff01f8ee39545e8d0526f6b5bb11922c",
+    "url": "/clicky-game/static/media/s1.ff01f8ee.jpg"
+  },
+  {
+    "revision": "d41c3571403a6dd3ca8f6d8d4bb623c3",
+    "url": "/clicky-game/static/media/s10.d41c3571.jpg"
+  },
+  {
+    "revision": "6ec4ed3f84d974a1cc70c56612cddbdd",
+    "url": "/clicky-game/static/media/s11.6ec4ed3f.jpg"
+  },
+  {
+    "revision": "c1cf40314c0a6b8125467a6f2b681a18",
+    "url": "/clicky-game/static/media/s12.c1cf4031.jpg"
+  },
+  {
+    "revision": "1635b5a67d81f6443052",
+    "url": "/clicky-game/static/js/main.1635b5a6.chunk.js"
+  },
+  {
+    "revision": "fd38c49080e3280dddd364d38d582ef2",
+    "url": "/clicky-game/static/media/s3.fd38c490.jpg"
+  },
+  {
+    "revision": "65c84ed1960a2f0777d03d149679a889",
+    "url": "/clicky-game/static/media/s4.65c84ed1.jpg"
+  },
+  {
+    "revision": "bf4a312d640f6960a471e705f937f348",
+    "url": "/clicky-game/static/media/s5.bf4a312d.jpg"
+  },
+  {
+    "revision": "cfca53574fbceecad7e33c8288e98bfe",
+    "url": "/clicky-game/static/media/s6.cfca5357.jpg"
+  },
+  {
+    "revision": "650bc66b35b552483f3cfdcfb98b4831",
+    "url": "/clicky-game/static/media/s7.650bc66b.jpg"
+  },
+  {
+    "revision": "fe4dc86a362f41bb0897e4bfd484f758",
+    "url": "/clicky-game/static/media/s8.fe4dc86a.jpg"
+  },
+  {
+    "revision": "03d95aa1677d19ee094e",
+    "url": "/clicky-game/static/css/1.220a96a6.chunk.css"
+  },
+  {
+    "revision": "ccbbb9115f83bc3281e510b4fdbf8d57",
+    "url": "/clicky-game/index.html"
+  }
+];
